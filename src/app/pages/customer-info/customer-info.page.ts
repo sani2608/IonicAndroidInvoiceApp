acbb8a88a64ed549af5538c298a9f38242defff4
sanikumar sahani <sanikumar.sahani@unvired.io>
2021-05-01 18:16:02 +0530

    :lipstick: add customer info in invoice

---
+++ src/app/pages/customer-info/customer-info.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-info',
+  templateUrl: './customer-info.page.html',
+  styleUrls: ['./customer-info.page.scss'],
+})
+export class CustomerInfoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
