33854338b81013cd4fac2961bdd4d00788619dde
sanikumar sahani <sanikumar.sahani@unvired.io>
2021-05-01 21:19:37 +0530

    :sparkles: shared utilities

---
+++ src/app/shared/toast.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+
+@NgModule({})
+export class Toast {
+  private isToastPresent = false; //prevent multiple toast bar
+  constructor(public toastController: ToastController) {}
+  /**
+   *
+   * @param message pass message accordingly
+   */
+  async displayToast(message: string) {
+    if (!this.isToastPresent) {
+      this.isToastPresent = true;
+      const toast = await this.toastController.create({
+        message,
+        position: 'top',
+        duration: 1000,
+        color: 'primary',
+      });
+      toast.present();
+      toast.onDidDismiss().then(() => (this.isToastPresent = false));
+    }
+  }
+}
