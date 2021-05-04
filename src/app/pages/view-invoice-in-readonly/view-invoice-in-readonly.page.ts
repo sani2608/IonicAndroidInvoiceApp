d75a4358ab529fa659de21ec3ffdb80a9b8b945e
sanikumar sahani <sanikumar.sahani@unvired.io>
2021-05-04 20:38:14 +0530

    show readonly invoice using ngIF

--- src/app/pages/view-invoice-in-readonly/view-invoice-in-readonly.page.ts
+++ src/app/pages/view-invoice-in-readonly/view-invoice-in-readonly.page.ts
@@ -7,11 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewInvoiceInReadonlyPage implements OnInit {
 
+  //TODO: to be replace later.
+  readOnlyItemList = [
+    {
+      cartId: 100,
+      name: 'Onion',
+      quantity: 23,
+      price: 20,
+      uom: 'kg',
+      totalPrice: 460,
+    },
+    {
+      cartId: 101,
+      name: 'Milk',
+      quantity: 5,
+      price: 30,
+      uom: 'Litre',
+      totalPrice: 150,
+    },
+    {
+      cartId: 102,
+      name: 'Cabbage',
+      quantity: 10,
+      price: 40,
+      uom: 'kg',
+      totalPrice: 400,
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
-  getAllItems(){
+  getAllItems() {
     //TODO: implement getAllItems() function.
   }
 
