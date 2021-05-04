fe55353e8fa8b679bc41105b9e54b59549e07034
sanikumar sahani <sanikumar.sahani@unvired.io>
2021-05-04 20:37:07 +0530

    show list using ngif & call alert for name

--- src/app/pages/add-invoice/add-invoice.page.ts
+++ src/app/pages/add-invoice/add-invoice.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Alert } from 'src/app/shared/alert';
+import { CustomerInfoPageModule } from '../customer-info/customer-info.module';
 
 @Component({
   selector: 'app-add-invoice',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-invoice.page.scss'],
 })
 export class AddInvoicePage implements OnInit {
-
-  constructor() { }
+  //TODO: to be replace with real data.
+  itemsInCart = [
+    {
+      cartId:100,
+      name: 'Onion',
+      quantity: 23,
+      price: 20,
+      uom: 'kg',
+      totalPrice: 460,
+    },
+    {
+      cartId:101,
+      name: 'Milk',
+      quantity: 5,
+      price: 30,
+      uom: 'Litre',
+      totalPrice: 150,
+    },
+    {
+      cartId:102,
+      name: 'Cabbage',
+      quantity: 10,
+      price: 40,
+      uom: 'kg',
+      totalPrice: 400,
+    },
+];
+  constructor(
+    private alert: Alert
+  ) { }
 
   ngOnInit() {
+    //run after 1 second delay.
+    setTimeout(() => {
+      this.alert.presentAlertPromptForCustomerInfo();
+    }, 1000);
+
+  }
+
+  getCustomerName(){
+    //TODO: implement getCustomerName() function.
   }
   getItemsFromCart(){
     //TODO: Implement getItemsFromCart() function.
