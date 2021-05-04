8b72fab3304cbaf97bc49295bc769ff5e89d12a4
sanikumar sahani <sanikumar.sahani@unvired.io>
2021-05-04 20:37:52 +0530

    show stocks  list using ngif

--- src/app/pages/stocks/stocks.page.ts
+++ src/app/pages/stocks/stocks.page.ts
@@ -7,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StocksPage implements OnInit {
 
+  //TODO: replace this with actual data from database.
+  items = [
+    {
+      name: 'onion',
+      price: 24,
+      uom: 'kg'
+    },
+    {
+      name: 'potato',
+      price: 30,
+      uom: 'kg'
+    },
+    {
+      name: 'milk',
+      price: 30,
+      uom: 'Litre'
+    },
+    {
+      name: 'cabbage',
+      price: 40,
+      uom: 'kg'
+    },
+    {
+      name: 'rice',
+      price: 100,
+      uom: 'kg'
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
+
   getAllItems(){
     //TODO: Implement getAllItems() function.
   }
