ace192c1916a7bdfe9907090d2ef128097e9c20c
sanikumar sahani <sanikumar.sahani@unvired.io>
2021-05-05 13:33:34 +0530

    minor changes

--- src/app/pages/stocks/stocks.page.ts
+++ src/app/pages/stocks/stocks.page.ts
@@ -10,26 +10,31 @@ export class StocksPage implements OnInit {
   //TODO: replace this with actual data from database.
   items = [
     {
+      itemId:1,
       name: 'onion',
       price: 24,
       uom: 'kg'
     },
     {
+      itemId:2,
       name: 'potato',
       price: 30,
       uom: 'kg'
     },
     {
+      itemId:3,
       name: 'milk',
       price: 30,
       uom: 'Litre'
     },
     {
+      itemId:4,
       name: 'cabbage',
       price: 40,
       uom: 'kg'
     },
     {
+      itemId:5,
       name: 'rice',
       price: 100,
       uom: 'kg'
