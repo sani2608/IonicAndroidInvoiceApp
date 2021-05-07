8d7b0a76afabddb77b66b67b9f486aa9808c2880
sanikumar sahani <sanikumar.sahani@unvired.io>
2021-05-07 15:09:16 +0530

    add item and update item apis

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Item } from '../models/data';
 
 @Injectable({
   providedIn: 'root'
@@ -9,20 +11,50 @@ export class DataService {
 
   //? Database related functions.
   //1. check if the database is present.
-  isDatabasePresent(){}
+  isDatabasePresent(){
+    //return boolean value if db is presesnt.
+    //use BehaviorSubject bolean.
+  }
+
+  createDatabase(){
+    // if isDatabasePresent returns false then create DATABASE.
+  }
+
+  //? Items Table
+  isItemPresent(value: string): boolean{
+    //TODO implement function to check if item is present in Database.
+    console.log('will check if '+ value + ' is present in Database');
+    return true;
+  }
+  addItem(value: Item): void {
+    //TODO: implement addItem function.
+    console.log('addItem from dataService\n', value);
+  }
+
+  updateItem(value: Item): void {
+    //TODO impmlement updateItemInDB() function.
+    console.log('updateItem from dataService\n',value);
+  }
+
+  getItems(): Observable<Item[]>  {
+    //TODO: implement getItemsFromDB() function.
+    //Use behavioural subject to pass latest item value.
+    //gets items list from DATABASE.
+    return;
+  }
+
+
+
+
+
+
+
+
+
+
 
 
   //*DATABASE CRUD FUNCTIONS.
-  //? Items Table
-  addItemInDB(){
-    //TODO: implement addItemInDB() function.
-  }
-  updateItemInDB(){
-    //TODO impmlement updateItemInDB() function.
-  }
-  getItemsFromDB(){
-    //TODO: implement getItemsFromDB() function.
-  }
   //? Invoice Table
   getInvoicesFromDB(){
     //TODO: implement getInvoicesFromDB() function.
