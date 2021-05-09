c34564e6212d7b89e7294f4527794545237e3e03
sanikumar sahani <sanikumar.sahani@unvired.io>
2021-05-09 19:09:14 +0530

    customer alertcontroller

--- src/app/pages/add-invoice/add-invoice.page.ts
+++ src/app/pages/add-invoice/add-invoice.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Customer } from 'src/app/models/data';
+import { Customer, Invoice } from 'src/app/models/data';
 import { DataService } from 'src/app/services/data.service';
 import { Alert } from 'src/app/shared/alert';
 
@@ -12,7 +12,7 @@ export class AddInvoicePage implements OnInit {
   //TODO: to be replace with real data.
   itemsInCart = [
     {
-      cartId:100,
+      cartId: 100,
       name: 'Onion',
       quantity: 23,
       price: 20,
@@ -20,7 +20,7 @@ export class AddInvoicePage implements OnInit {
       totalPrice: 460,
     },
     {
-      cartId:101,
+      cartId: 101,
       name: 'Milk',
       quantity: 5,
       price: 30,
@@ -28,14 +28,14 @@ export class AddInvoicePage implements OnInit {
       totalPrice: 150,
     },
     {
-      cartId:102,
+      cartId: 102,
       name: 'Cabbage',
       quantity: 10,
       price: 40,
       uom: 'kg',
       totalPrice: 400,
     },
-];
+  ];
   constructor(
     private alert: Alert,
     private dataService: DataService
@@ -43,19 +43,61 @@ export class AddInvoicePage implements OnInit {
 
   ngOnInit() {
     //run after 1 second delay.
+    this.captureCustomerName(1000);
+    this.createNewInvoice();
+  }
+
+  /**
+   * @param delayTime is passed to function to open the alert after some delay.
+   */
+  captureCustomerName(delayTime: number) {
+    const inputObj = [
+      {
+        name: 'firstName',
+        type: 'text',
+        placeholder: 'sanikumar'
+      },
+      {
+        name: 'lastName',
+        type: 'text',
+        placeholder: 'sahani'
+      },
+    ];
+    const buttonObj = [
+        {
+          text: 'Submit',
+          handler: (value) => {
+            console.log('Confirm Submit');
+            console.log(value.firstName + ' ' + value.lastName);
+          }
+        }
+      ];
+      const header = 'Enter Customer Name';
     setTimeout(() => {
-      this.alert.presentAlertPromptForCustomerInfo();
-    }, 1000);
-
+      this.alert.presentAlertPrompt(
+        header,
+        inputObj,
+        buttonObj
+      );
+    }, delayTime);
   }
 
-  addCustomerName(value: Customer): void {
+  /**
+   * AS soon as the customer taps on Add new invoice, a new row is
+   *  inserted in the table with just invoiceId and created Date.
+   */
+  createNewInvoice() {
+    //as soon as the customer goes to add new Invoice page an, Invoice_no will be generated.
+  }
+
+  addCustomerToInvoice(customerName: Customer, invoiceNumber: Invoice): void {
     //?get value from the alert and pass it to service.
-    this.dataService.addCustomer(value);
+    //Here Only customerName will be passed because the invoiceId in automatically generated as it is a primary key
+    this.dataService.addCustomerToInvoice(customerName, invoiceNumber);
   }
 
-  deleteItemFromInvoice(itemId: number): void {
-    this.dataService.deleteItemFromInvoice(itemId);
+  deleteItemFromInvoice(itemId: number, invoiceNumber: number): void {
+    this.dataService.deleteItemFromInvoice(itemId, invoiceNumber);
   }
 
 }
