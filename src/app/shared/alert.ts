ae777c2f994230ea030da3733c6f18975b75c0de
sanikumar sahani <sanikumar.sahani@unvired.io>
2021-05-09 19:07:53 +0530

    made Alertcontroller reusable by other components

--- src/app/shared/alert.ts
+++ src/app/shared/alert.ts
@@ -6,65 +6,12 @@ export class Alert {
 
   constructor(public alertController: AlertController) { }
 
-  async presentAlertPrompt() {
+  async presentAlertPrompt(header: string, inputs?: any, buttons?: any) {
     const alert = await this.alertController.create({
-      cssClass: 'my-custom-class',
-      header: 'Enter Quantity',
-      inputs: [
-        {
-          name: 'quantity',
-          type: 'number',
-          placeholder: '00'
-        },
-      ],
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'cancel',
-          cssClass: 'secondary',
-          handler: () => {
-            console.log('Confirm Cancel');
-          }
-        }, {
-          text: 'Ok',
-          handler: (value) => {
-            console.log('Confirm Ok');
-            console.log(value);
-            return value.quantity;
-          }
-        }
-      ]
-    });
-
-    await alert.present();
-  }
-
-  //for customer information
-  async presentAlertPromptForCustomerInfo() {
-    const alert = await this.alertController.create({
-      cssClass: 'my-custom-class-customer',
-      header: 'Enter Customer Name',
-      inputs: [
-        {
-          name: 'firstName',
-          type: 'text',
-          placeholder: 'sanikumar'
-        },
-        {
-          name: 'lastName',
-          type: 'text',
-          placeholder: 'sahani'
-        },
-      ],
-      buttons: [
-        {
-          text: 'Submit',
-          handler: (value) => {
-            console.log('Confirm Ok');
-            console.log(value.firstName+' '+ value.lastName);
-          }
-        }
-      ]
+      cssClass: 'alert-class',
+      header,
+      inputs,
+      buttons,
     });
 
     await alert.present();
