a6101a9081c9b80b2a6721fc3d80477187424160
sanikumar sahani <sanikumar.sahani@unvired.io>
2021-05-10 20:37:32 +0530

    data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,16 +1,46 @@
+/* eslint-disable no-underscore-dangle */
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { Customer, Invoice, Invoices, Item, ReadOnlyInvoice } from '../models/data';
+import { Customer, Invoice, Invoices, Item, ItemAddedInNewInvoice, ReadOnlyInvoice } from '../models/data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  //? Private variables to store data.
+  /** this variable will store all the items present in the stock */
   private _listOfItemsInStock: Array<Item[]> = [];
+  /**this will store all the invoice to show on homepage */
   private _homePageInvoiceList: Array<Invoice[]> = [];
-  private _temporaryItemList: Array<Item[]> = [];
+  /** This will store the items that are added in stock while creating new invoice */
+  private _itemsAddedInNewInvoice: Array<ItemAddedInNewInvoice[]> = [];
+  /** This will store the ready only invoice details */
+  private _readOnlyInvoiceDetails: ReadOnlyInvoice;
 
-  constructor() { }
+  //? Getters and Setters for private variables
+  /** Getter itemsAddedInNewInvoice */
+  public get itemsAddedInNewInvoice(): Array<ItemAddedInNewInvoice[]> {
+    return this._itemsAddedInNewInvoice;
+  }
+
+  /** Getter readOnlyInvoiceDetails */
+  public get readOnlyInvoiceDetails(): ReadOnlyInvoice {
+    return this._readOnlyInvoiceDetails;
+  }
+
+  /** Getter listOfItemsInStock */
+  public get listOfItemsInStock(): Array<Item[]> {
+    return this._listOfItemsInStock;
+  }
+
+  /**Getter homePageInvoiceList */
+  public get homePageInvoiceList(): Array<Invoice[]> {
+    return this._homePageInvoiceList;
+  }
+
+  /**Getter itemsAddedInInvoice*/
+  public get itemsAddedInInvoice(): Array<ItemAddedInNewInvoice[]> {
+    return this._itemsAddedInNewInvoice;
+  }
 
   //? Database related functions.
   async isDatabasePresent(): Promise<boolean> {
@@ -18,47 +48,42 @@ export class DataService {
     return false;
   }
 
-  async createDatabase() {
+  async createDatabase(): Promise<any> {
     // if isDatabasePresent returns false then create DATABASE.
   }
 
   //? Items Table
-  async isItemPresent(itemName: string): Promise<boolean> {
+  async isItemPresentInStock(itemName: string): Promise<boolean> {
     console.log('will check if ' + itemName + ' is present in Database');
     return true;
   }
 
-  async addItem(item: Item): Promise<any> {
+  async addItemInStock(item: Item): Promise<any> {
     console.log('addIem from dataService\n', item);
   }
 
-  async updateItem(item: Item, itemId: number): Promise<any> {
+  async updateItemInStock(item: Item, itemId: number): Promise<any> {
     console.log('updateItem from dataService\n', item, itemId);
   }
 
   /**
-   *
    * @param itemId is the itemId of the item
    * @returns item
    */
-  async getItemByItemId(itemId: number): Promise<Item[]> {
-    //gets items list from DATABASE.
+  async getItemByItemIdFromStock(itemId: number): Promise<Item[]> {
     console.log('from dataservice', itemId);
     return;
   }
 
   /**
-   * @returns list of items present in the stock.
-   */
-  async getItems(): Promise<Item[]> {
+   * @returns list of items present in the stock */
+  async getListOfItemsFromStock(): Promise<Item[]> {
     //gets items list from DATABASE.
     return;
   }
 
-
-
   //? HOME SECTION
-  async getInvoices(): Promise<Invoices[]> {
+  async getAllInvoices(): Promise<Invoices[]> {
     return;
   }
 
@@ -70,25 +95,31 @@ export class DataService {
     console.log('from search by invoiceNumber \n', invoiceNumber);
   }
 
+
   //? READONLY PAGE
-  async getInvoicesInReadOnly(): Promise<ReadOnlyInvoice[]> {
-    return;
-  }
-  //? ADD NEW INVOICE
-  async addCustomerToInvoice(customerName: Customer, invoice: Invoice): Promise<any> {
+  async getInvoiceDetailsByInvoiceId(invoiceId: number): Promise<ReadOnlyInvoice[]> {
     return;
   }
 
-  async addItemInInvoice(itemId: number, quantity: number, invoice: number): Promise<any> {
+  //? NEW INVOICE PAGE RELATED FUNCTIONS
+  async createNewInvoice(date: Date): Promise<any> {
     return;
   }
 
-  async getItemsFromInvoice(): Promise<ReadOnlyInvoice[]> {
+  async addCustomerInNewInvoice(customerName: Customer, invoiceNumber: number): Promise<any> {
+    return;
+  }
+
+  async addItemInNewInvoice(itemId: number, quantity: number, invoice: number): Promise<any> {
+    return;
+  }
+
+  async getItemsFromNewInvoice(): Promise<ReadOnlyInvoice[]> {
     //will get items that are added in the cart.
     return;
   }
 
-  async deleteItemFromInvoice(itemId: number, invoiceNumber: number): Promise<any> {
+  async deleteItemFromNewInvoice(itemId: number, invoiceNumber: number): Promise<any> {
     return;
   }
 }
