2a259a4247bbedba9c296ffd1c8eee93ad3403f6
sanikumar sahani <sanikumar.sahani@unvired.io>
2021-05-16 04:17:26 +0530

    subscribing to invoice data on homepage

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -8,56 +8,28 @@ import { DataService } from 'src/app/services/data.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  //TODO: replace this with actual data from database.
-  invoices = [
-    {
-      customerName: 'John Doe',
-      date: 'April 25, 2021',
-      totalItems: 10,
-      totalPrice: 1000,
-      invoiceId: 1000,
-
-    },
-    {
-      customerName: 'Soumya',
-      date: 'April 25, 2021',
-      totalItems: 15,
-      totalPrice: 1200,
-      invoiceId: 1001,
-    },
-    {
-      customerName: 'Girish',
-      date: 'April 30, 2021',
-      totalItems: 20,
-      totalPrice: 1500,
-      invoiceId: 1002,
-    },
-    {
-      customerName: 'Sanikumar',
-      date: 'May 3, 2021',
-      totalItems: 35,
-      totalPrice: 5600,
-      invoiceId: 1003,
-    },
-    {
-      customerName: 'Saurabh',
-      date: 'May 3, 2021',
-      totalItems: 30,
-      totalPrice: 5000,
-      invoiceId: 1004,
-    },
-  ];
-
+ 
 
   constructor(
     private dataService: DataService
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getAllInvoices();
+  }
+  public get invoices(){
+    return this.dataService.homePageInvoiceList;
+  }
 
 
   public getAllInvoices(): void {
-    this.dataService.getAllInvoices();
+    this.dataService.databaseState()
+      .subscribe((response) => {
+        console.log('getting invoice on homepage .....\n', response)
+        if (response) {
+          this.dataService.getAllInvoices();
+        }
+      });
   }
 
   public searchInvoice(searchValue: string): void {
