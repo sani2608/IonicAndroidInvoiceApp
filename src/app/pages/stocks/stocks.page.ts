0b54e1e68a4b968b532a1806b0a06d1f527eb066
sanikumar sahani <sanikumar.sahani@unvired.io>
2021-05-19 20:32:23 +0530

    stock page stores data of stocks instead of dataservice

--- src/app/pages/stocks/stocks.page.ts
+++ src/app/pages/stocks/stocks.page.ts
@@ -1,9 +1,11 @@
 /* eslint-disable no-underscore-dangle */
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Cart } from 'src/app/models/data';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { Cart, Item } from 'src/app/models/data';
 import { DataService } from 'src/app/services/data.service';
 import { Alert } from 'src/app/shared/alert';
+
 
 @Component({
   selector: 'app-stocks',
@@ -11,6 +13,7 @@ import { Alert } from 'src/app/shared/alert';
   styleUrls: ['./stocks.page.scss'],
 })
 export class StocksPage implements OnInit {
+  private _listOfItemsInStock: Array<Item> = [];
   private _flag = false;
   private _isStocksPageOrAddItemInInvoicePage: string;
   private invoiceId: number;
@@ -20,35 +23,38 @@ export class StocksPage implements OnInit {
     private dataService: DataService,
     private route: ActivatedRoute,
     private alert: Alert,
-  ) { }
+    public navCtrl: NavController) {
+    route.params.subscribe(() => {
+      // put the code from `ngOnInit` here
+      console.log('constructor inside Stocks');
+      this.getAllItems();
+    });
+  }
 
   ngOnInit() {
     this.getStatusOfUrl();
-    this.getAllItems();
-    console.log('ngOnInit lifecycle inside Stocks');
   }
 
-    /**
-     * Getter _isStocksPageOrAddItemInInvoicePage @return {string}
-     */
-	public get isStocksPageOrAddItemInInvoicePage(): string {
-		return this._isStocksPageOrAddItemInInvoicePage;
-	}
+  /**
+   * Getter _isStocksPageOrAddItemInInvoicePage @return {string}
+   */
+  public get isStocksPageOrAddItemInInvoicePage(): string {
+    return this._isStocksPageOrAddItemInInvoicePage;
+  }
+
+  /**
+   * Getter listOfItemsInStock @return {Array<Item> }
+   */
+  public get itemsInStock(): Array<Item> {
+    return this._listOfItemsInStock;
+  }
+
   /** Getter itemsInStock used by template */
   public get flag() {
     return this._flag;
   }
 
-
-
-  /** Getter itemsInStock used by template */
-  public get itemsInStock() {
-    return this.dataService.listOfItemsInStock;
-  }
-
-
   public showAlertBox(itemId: number, itemPrice: number) {
-    console.log('itemId which is going to be added in cart \n', itemId , itemPrice);
     const inputObj = [
       {
         name: 'quantity',
@@ -64,17 +70,18 @@ export class StocksPage implements OnInit {
         handler: () => {
           console.log('Confirm Cancel');
         }
-      }, {
+      },
+      {
         text: 'Ok',
         handler: (quantity: any) => {
-          const quantityNumber = parseInt(quantity.quantity,10);
+          const quantityNumber = parseInt(quantity.quantity, 10);
           const cartItem = new Cart();
           cartItem.invoiceId = this.invoiceId;
           cartItem.itemId = itemId;
           cartItem.quantity = quantityNumber;
           cartItem.buyPrice = itemPrice;
           this.addItemInInvoice(cartItem);
-          this.getAllItemsInNewInvoice(this.invoiceId);
+          this.getAllItemsInNewInvoice(itemId);
         }
       }
     ];
@@ -92,22 +99,25 @@ export class StocksPage implements OnInit {
   }
 
   /** This will get all the items present in the stock from Database */
-  private getAllItems() {
+  private getAllItems(): void {
     this.dataService.databaseState()
       .subscribe((response) => {
         if (response) {
-          this.dataService.getListOfItemsFromStock();
+          this.dataService.getListOfItemsFromStock().then(
+            responseArray => this._listOfItemsInStock = responseArray
+          );
         }
       });
   }
+
   /** this function will check if the route if toupdate item or add new item*/
   private getStatusOfUrl(): void {
     this._isStocksPageOrAddItemInInvoicePage = this.route.snapshot.params.s;
     if (this._isStocksPageOrAddItemInInvoicePage === 's') {
       this._flag = true;
     } else {
-      this.invoiceId = parseInt(this._isStocksPageOrAddItemInInvoicePage,10);
-      console.log('invoice id is',this.invoiceId);
+      this.invoiceId = parseInt(this._isStocksPageOrAddItemInInvoicePage, 10);
+      console.log('invoice id is', this.invoiceId);
       this._flag = false;
     }
   }
