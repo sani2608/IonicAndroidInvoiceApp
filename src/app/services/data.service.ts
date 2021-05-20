e30138bbfb845406d93d7fde82af8b43b8f353e1
sanikumar sahani <sanikumar.sahani@unvired.io>
2021-05-20 23:08:59 +0530

    removed then() and unwanted comments

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,10 +1,10 @@
 /* eslint-disable max-len */
 /* eslint-disable no-underscore-dangle */
 import { Injectable } from '@angular/core';
-import { Cart, Customer, Invoices, Item, ItemAddedInNewInvoice } from '../models/data';
 import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
 import { Platform } from '@ionic/angular';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { Cart, Customer, Invoices, Item, ItemAddedInNewInvoice } from '../models/data';
 import { itemTableQuery, customerTableQuery, cartTableQuery, invoiceTableQuery, triggerQuery, CustomQueries } from '../services/sqlQueries/queries';
 @Injectable({
   providedIn: 'root'
@@ -27,13 +27,13 @@ export class DataService {
     });
   }
 
-
-
   //? DATABASE RELATED FUNCTIONS
+  /** @returns boolean value for database ready for operation or not */
   databaseState() {
     return this.isDatabaseReady.asObservable();
   }
 
+  /** Creates the Database and tables respectively */
   async createDatabase(): Promise<void> {
     await this.sqlite.create({
       name: 'invoice.db',
@@ -55,11 +55,16 @@ export class DataService {
   }
 
   //? ITEMS TABLE RELATED FUNCTIONS
+  /** @param item is passed to database */
   async addItemInStock(item: Item): Promise<void> {
     const itemDataArrayToBePassedInQuery: Array<string | number> = [item.name.toLowerCase(), item.price, item.uom];
     await this.databaseObject.executeSql(this.customQueries.insertNewItem(), itemDataArrayToBePassedInQuery);
   }
 
+  /**
+   * @param item is item that will get updated
+   * @param itemId based on itemid item will be updated
+   */
   async updateItemInStock(item: Item, itemId: number): Promise<void> {
     const itemDataArrayToBePassedInQuery: Array<string | number> = [item.name.toLowerCase(), item.price, item.uom];
     await this.databaseObject.executeSql(this.customQueries.updateItemById(itemId), itemDataArrayToBePassedInQuery);
@@ -70,14 +75,12 @@ export class DataService {
    * @returns single item Object
    */
   async getItemByItemIdFromStock(itemId: number): Promise<Item> {
+    const response = await this.databaseObject.executeSql(this.customQueries.getItemById(itemId), []);
     const item = new Item();
-    await this.databaseObject.executeSql(this.customQueries.getItemById(itemId), [])
-      .then((response) => {
-        item.itemId = response.rows.item(0).item_id;
-        item.name = response.rows.item(0).name;
-        item.uom = response.rows.item(0).uom;
-        item.price = response.rows.item(0).price;
-      });
+    item.itemId = response.rows.item(0).item_id;
+    item.name = response.rows.item(0).name;
+    item.uom = response.rows.item(0).uom;
+    item.price = response.rows.item(0).price;
     return item;
   }
 
@@ -85,119 +88,122 @@ export class DataService {
    * @returns list of items present in the stock
    */
   async getListOfItemsFromStock(): Promise<Item[]> {
+    const res = await this.databaseObject.executeSql(this.customQueries.getItemsFromStock(), [])
+      .catch((response) => console.log(response));
     const itemsArray: Array<Item> = [];
-    await this.databaseObject.executeSql(this.customQueries.getItemsFromStock(), [])
-      .then((res) => {
-        if (res.rows.length > 0) {
-          for (let i = 0; i < res.rows.length; i++) {
-            const newItemData = new Item(
-              res.rows.item(i).item_id,
-              res.rows.item(i).name,
-              res.rows.item(i).price,
-              res.rows.item(i).uom);
-            itemsArray.push(newItemData);
-          }
-        }
+    if (res.rows.length > 0) {
+      for (let i = 0; i < res.rows.length; i++) {
+        const newItemData = new Item(
+          res.rows.item(i).item_id,
+          res.rows.item(i).name,
+          res.rows.item(i).price,
+          res.rows.item(i).uom);
+        itemsArray.push(newItemData);
       }
-      ).catch((response) => console.log(response));
+    }
     return itemsArray;
   }
 
   //? HOME SECTION RELATED FUNCTION
+  /** @returns list of all invoices present in databae */
   async getAllInvoices(): Promise<Invoices[]> {
+    const response = await this.databaseObject.executeSql(this.customQueries.getInvoicesForHomePage(), [])
+      .catch(() => 'Got Error while getting invoices');
     const invoiceArray: Array<Invoices> = [];
-    await this.databaseObject.executeSql(this.customQueries.getInvoicesForHomePage(), [])
-      .then((resp) => {
-        if (resp.rows.length > 0) {
-          for (let i = 0; i < resp.rows.length; i++) {
-            const newInvoiceData = new Invoices(
-              resp.rows.item(i).customer_full_name,
-              resp.rows.item(i).total_items_in_cart,
-              resp.rows.item(i).invoice_id,
-              resp.rows.item(i).created_date,
-              resp.rows.item(i).total_price);
-            invoiceArray.push(newInvoiceData);
-          }
-        }
+    if (response.rows.length > 0) {
+      for (let i = 0; i < response.rows.length; i++) {
+        const newInvoiceData = new Invoices(
+          response.rows.item(i).invoice_id,
+          response.rows.item(i).created_date,
+          response.rows.item(i).total_price,
+          response.rows.item(i).total_items_in_cart,
+          response.rows.item(i).customer_full_name,
+        );
+        invoiceArray.push(newInvoiceData);
       }
-      ).catch((response) => console.log(response));
+    }
     return invoiceArray;
   }
 
   //? READONLY PAGE  FUNCTION
+  /**
+   * @param invoiceId is passed to get the data
+   * @returns Invoice object
+   */
   async getInvoiceDetailsByInvoiceId(invoiceId: number): Promise<Invoices> {
-    const readOnlyInvoice: Invoices = new Invoices();
-    await this.databaseObject.executeSql(this.customQueries.getReadOnlyInvoiceDetailsById(invoiceId), [])
-      .then(
-        (response) => {
-          readOnlyInvoice.customerFullName = response.rows.item(0).customer_full_name;
-          readOnlyInvoice.totalItems = response.rows.item(0).total_items_in_cart;
-          readOnlyInvoice.invoiceId = response.rows.item(0).invoice_id;
-          readOnlyInvoice.createdDate = response.rows.item(0).created_date;
-          readOnlyInvoice.totalPrice = response.rows.item(0).total_price;
-        }).catch((e) => console.log('got error while getting invoide', e));
-    return readOnlyInvoice;
+    const response = await this.databaseObject.executeSql(this.customQueries.getReadOnlyInvoiceDetailsById(invoiceId), []);
+    const invoice: Invoices = new Invoices();
+    invoice.invoiceId = response.rows.item(0).invoice_id;
+    invoice.createdDate = response.rows.item(0).created_date;
+    invoice.totalPrice = response.rows.item(0).total_price;
+    invoice.totalItems = response.rows.item(0).total_items_in_cart;
+    invoice.customerFullName = response.rows.item(0).customer_full_name;
+    console.log('got invoice from db', invoice);
+    return invoice;
   }
-
 
   //? NEW INVOICE PAGE RELATED FUNCTIONS
+  /**
+   * @param customerId is passed to new invoice
+   * @returns invoiceId after invoice is created
+   */
   async createNewInvoice(customerId: number): Promise<number> {
-    let invoiceNumber: number;
-    await this.databaseObject.executeSql(this.customQueries.createNewInvoice(customerId), [])
-      .then((res) => {
-        console.log('New Invoice Created Successfully.. with Id = ', res.insertId);
-        invoiceNumber = res.insertId;
-      }).catch((e) => console.log('Error while creating New Invoice..', e));
-    return invoiceNumber;
+    const invoiceId = await this.databaseObject.executeSql(this.customQueries.createNewInvoice(customerId), [])
+      .catch((e) => console.log('Error while creating New Invoice..', e));
+    return invoiceId.insertId;
   }
-
 
   //? CUSTOMER TABLE
+  /**
+   * @param customerName is added to customer table
+   * @returns customerId after customer name is inserted into table
+   */
   async addCustomer(customerName: Customer): Promise<number> {
-    let customerId: number;
-    console.log('adding customer to database', customerName.fullName);
-    await this.databaseObject.executeSql(this.customQueries.addCustomer(),
-      [customerName.firstName, customerName.lastName])
-      .then(res => customerId = res.insertId)
+    const customer = await this.databaseObject
+      .executeSql(this.customQueries.addCustomer(),
+        [customerName.firstName, customerName.lastName])
       .catch(err => console.log('Got error while adding customer', err));
-    return customerId;
+    return customer.insertId;
   }
 
 
+  /** @param item is added to cart */
   async addItemInNewInvoice(item: Cart): Promise<void> {
-    const itemDataArrayToBePassedInQuery: Array<number> = [item.invoiceId, item.itemId, item.buyPrice, item.quantity, item.totalItemPrice];
-    await this.databaseObject.executeSql(this.customQueries.addItemToCart(), itemDataArrayToBePassedInQuery)
-      .then((response) => {
-        console.log('Item added successfully in Cart ', response);
-      })
+    const itemDataArrayToBePassedInQuery:
+      Array<number> = [item.invoiceId, item.itemId, item.buyPrice, item.quantity, item.totalItemPrice];
+    await this.databaseObject.
+      executeSql(this.customQueries.addItemToCart(), itemDataArrayToBePassedInQuery)
       .catch(err => console.log('Got error while adding item to cart', err));
   }
 
+  /**
+   * @param invoiceId is passed to database to get items in cart
+   * @returns returns list of items present in the invoiceId
+   */
   async getItemsFromNewInvoice(invoiceId: number): Promise<ItemAddedInNewInvoice[]> {
+    const res = await this.databaseObject.executeSql(this.customQueries.getItemsFromCartByInvoiceId(invoiceId), []);
     const itemsInInvoiceArray: Array<ItemAddedInNewInvoice> = [];
-    await this.databaseObject.executeSql(this.customQueries.getItemsFromCartByInvoiceId(invoiceId), [])
-      .then((res) => {
-        console.log('got response from get new items from cart', res);
-        if (res.rows.length > 0) {
-          for (let i = 0; i < res.rows.length; i++) {
-            const newItemData = new ItemAddedInNewInvoice(
-              res.rows.item(i).item_id,
-              res.rows.item(i).name,
-              res.rows.item(i).price,
-              res.rows.item(i).uom,
-              res.rows.item(i).quantity,
-              res.rows.item(i).total_item_price,
-            );
-            itemsInInvoiceArray.push(newItemData);
-          }
-        }
+    if (res.rows.length > 0) {
+      for (let i = 0; i < res.rows.length; i++) {
+        const newItemData = new ItemAddedInNewInvoice(
+          res.rows.item(i).item_id,
+          res.rows.item(i).name,
+          res.rows.item(i).price,
+          res.rows.item(i).uom,
+          res.rows.item(i).quantity,
+          res.rows.item(i).total_item_price,
+        );
+        itemsInInvoiceArray.push(newItemData);
       }
-      ).catch((response) => console.log('got error while fetching items\n', response));
-    console.log(itemsInInvoiceArray);
+    }
     return itemsInInvoiceArray;
   }
 
-  async deleteItemFromNewInvoice(itemId: number, invoiceId: number, index: number): Promise<void> {
+  /**
+   * @param itemId is passed to database to delete the item
+   * @param invoiceId is passed to database to delete the item
+   */
+  async deleteItemFromNewInvoice(itemId: number, invoiceId: number): Promise<void> {
     await this.databaseObject
       .executeSql(this.customQueries.deleteCartItemByInvoiceIdAndItemId(itemId, invoiceId), [])
       .then((res) => {
